a681ae2c2d1290c76ec6e46b355b504776ab2682
yse297 <yse297@gmail.com>
2023-03-08 18:02:30 +0900

    last

---
+++ WebProgramming/Source/06_jsp/ch19_mvcBoard/src/com/lec/ex/service/BContentService.java
@@ -0,0 +1,18 @@
+package com.lec.ex.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.lec.ex.dao.BoardDao;
+
+public class BContentService implements Service {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		int bid = Integer.parseInt(request.getParameter("bid"));
+		BoardDao bDao = new BoardDao();
+		request.setAttribute("contentBoard", bDao.content(bid));
+	}
+
+}
